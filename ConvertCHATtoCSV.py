1165b9e4e341ff26856dac9eaa940ebc928258d4
therndon13 <45801768+therndon13@users.noreply.github.com>
2018-12-11 15:30:02 -0600

    Update ConvertCHATtoCSV.py

--- ConvertCHATtoCSV.py
+++ ConvertCHATtoCSV.py
@@ -9,7 +9,7 @@ import zipfile
 import os
 
 #CHAT2
-data_xls = pd.read_excel('C:\DataExtracts\SprintFiles\CHAT2.xlsx', header=1, index_col=0)
-data_xls.to_csv('C:\DataExtracts\SprintFiles\CHAT2.csv',sep='↑', encoding='utf-8')
+data_xls = pd.read_excel('C:\DataExtracts\Book1.xlsx', header=1, index_col=0)
+data_xls.to_csv('C:\DataExtracts\Book1.csv',sep='↑', encoding='utf-8')
 
 print('winner winner chicken dinner')
